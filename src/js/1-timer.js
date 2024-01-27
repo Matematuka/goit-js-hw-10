ee116917ef1298d8b51a5669e1bbf2b12dd790bf
Matematuka <143654489+Matematuka@users.noreply.github.com>
2024-01-27 19:44:56 +0200

    library

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,3 +1,35 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+
+const dateInput = document.querySelector("#datetime-picker");
+const startBtn = document.querySelector("button");
+
+
+let userSelectedDate;
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    userSelectedDate = selectedDates;
+    console.log(selectedDates[0]);
+  },
+};
+const fp = flatpickr(dateInput, options);
+
+
+iziToast.show({
+  title: 'Error',
+  message: 'Please choose a date in the future',
+  backgroundColor: '#EF4040',
+  titleColor: '#FFF',
+  titleSize: '16px',
+  messageColor: '#FFF',
+  messageSize: '16px',
+  position: 'topCenter'
+});
 
